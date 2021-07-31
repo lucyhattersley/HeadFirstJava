003e856b853ca1b06fdd3d935d02238f45f9773d
Lucy Hattersley <lucy@Lucys-MacBook-Air.local>
2021-07-31 18:51:55 +0100

    updated SimpleDotCom

---
+++ DotComBust/src/SimpleDotCom.java
@@ -0,0 +1,60 @@
+
+public class SimpleDotCom {
+	
+	String stringGuess;
+	int[] locationCells;
+	int numOfHits = 0;
+
+	public void setLocationCells(int[] locs) {
+		locationCells = locs;
+	}
+
+	public String checkYourself(String stringGuess) {
+		int guess = Integer.parseInt(stringGuess);
+		String result = "miss";
+		
+		for (int cell : locationCells) {
+			if (guess == cell) {
+				result = "hit";
+				numOfHits++;
+				break;
+			} // end if
+		} // end for
+		
+		if (numOfHits == locationCells.length) {
+			result = "kill";
+			
+		} // end if
+		
+		System.out.println(result);
+		return result;
+	} // end method
+	
+	public static void main(String[] args) {
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		int randomNum = (int) (Math.random() * 5);
+		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		
+		theDotCom.setLocationCells(locations);
+		boolean isAlive = true;
+		
+		while(isAlive == true) {
+			String guess = helper.getUserInput("enter a number");
+			String result = theDotCom.checkYourself(guess);
+			
+			numOfGuesses++;
+			
+			if (result.equals("kill")) {
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses");
+				
+			} // close if
+		} // close while
+		
+		
+		
+	} // close main
+	
+}
